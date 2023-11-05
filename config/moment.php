494d9c19fe64bc454929a7f054b55e50631c6db3
CyberPunkCodes <9557392+CyberPunkCodes@users.noreply.github.com>
2023-11-05 03:16:29 -0500

    add script assets

--- config/moment.php
+++ config/moment.php
@@ -2,4 +2,10 @@
 
 return [
     'prefix' => '',
+    'assets' => [
+        'scripts' => [
+            '<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js" integrity="sha512-+H4iLjY3JsKiF2V6N366in5IQHj2uEsGV7Pp/GRcm0fn76aPAk5V8xB6n8fQhhSonTqTXs/klFz4D0GIn6Br9g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>',
+            '<script src="https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.43/moment-timezone-with-data.min.js" integrity="sha512-KCI+fR3bUbOcU0ZC3UcaPwCLuO5LEkukaWBYddmhLPXgpAPWJ8j6pJLmTI6t9CMYczE4YCrWZQ/wrZz/JsyC+A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>'
+        ],
+    ],
 ];
